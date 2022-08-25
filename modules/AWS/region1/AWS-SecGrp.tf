75af731abaaa30b0fe79e6b7d22e2851ed27ef86
Deibys Campos Rivero <dcamposr1979@gmail.com>
2022-08-24 19:35:39 -0500

    Changing Security Group

--- modules/AWS/region1/AWS-SecGrp.tf
+++ modules/AWS/region1/AWS-SecGrp.tf
@@ -35,14 +35,14 @@ resource "aws_security_group" "PNet-MainVPC"{
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["10.0.0.0/16", "10.1.0.0/16", "10.2.0.0./16"]        
+        cidr_blocks = ["10.0.0.0/16", "10.1.0.0/16"]        
     }
 
     ingress {
         from_port = 8
         to_port = 0
         protocol = "icmp"
-        cidr_blocks = ["10.0.0.0/16", "10.1.0.0/16", "10.2.0.0./16"]        
+        cidr_blocks = ["10.0.0.0/16", "10.1.0.0/16"]        
     }
 
     egress  {
